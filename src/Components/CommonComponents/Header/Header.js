71d907a0080de8ef0a491b1a955bf7078646b725
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-04-06 21:54:37 +0530

    upload finished

--- src/Components/CommonComponents/Header/Header.js
+++ src/Components/CommonComponents/Header/Header.js
@@ -1,18 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./header.css";
 import bell from "../../../assets/images/new.svg";
+
 import SearchIcon from "@mui/icons-material/Search";
 import { isExpired, decodeToken } from "react-jwt";
 import { searchContext } from "../../../Context/searchContext";
+
 export default function Header() {
   var [decodedData, setFullData] = useState();
+  const [imgpath, setImgPath] = useState();
   const { searchVariable, updateSearchVariable } = useContext(searchContext);
   const handleChange = (e) => {
     updateSearchVariable(e.target.value);
   };
+
   useEffect(() => {
     var token = localStorage.getItem("token");
+    console.log(decodeToken(token));
     setFullData(decodeToken(token));
+    setImgPath("");
   }, []);
   return (
     <div>
@@ -25,11 +31,11 @@ export default function Header() {
         ></input>
         <div className="user-details">
           <img className="notification" src={bell}></img>
-          <p className="user-name">{decodedData?.FullName}</p>
+          <p className="user-name">{decodedData?.userexist.FullName}</p>
 
           <div className="dp">
             <img
-              src={decodedData?.avatar}
+              src={`/${decodedData?.userexist.avatar}`}
               style={{ height: "45px", width: "45px" }}
             ></img>
           </div>
--- src/Components/General /General.js
+++ src/Components/General /General.js
@@ -3,12 +3,24 @@ import "./general.css";
 import Box from "@mui/material/Box";
 import { InputLabel } from "@mui/material";
 import ChartCard from "../CommonComponents/ChartCard/ChartCard";
+import axios from "axios";
 export default function General(props) {
+  const [selectedFile, setFile] = useState();
   const handleChange = (e) => {
     props.state.setProductData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
+  };
+  const changeFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+  var formData = new FormData();
+  const upload = () => {
+    formData.append("myFile", selectedFile);
+    axios.post("http://localhost:8080/upload", formData).then((resp) => {
+      console.log(resp.data);
+    });
   };
   return (
     <div style={{ display: "flex" }}>
@@ -78,10 +90,18 @@ export default function General(props) {
       </Box>
       <div
         className="chart-container"
-        style={{ width: "50%", height: "395px" }}
+        style={{ width: "50%", height: "415px" }}
       >
         <h3>Media</h3>
-        <input type="file" className="upload"></input>
+        <input
+          type="file"
+          className="upload"
+          name="myFile"
+          onChange={changeFile}
+        ></input>
+        <button className="action-img save" onClick={upload}>
+          Upload
+        </button>
       </div>
     </div>
   );
